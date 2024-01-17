5dc24a12438086995e9af4f5faac2bc91783a970
slavikx4 <110037734+slavikx4@users.noreply.github.com>
2024-01-18 02:56:57 +0300

    update main.py
    второй комит

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import re
 
 def display_letters(input_string):    letters_only = re.findall(r'[a-zA-Z]', input_string)
     result = ''.join(letters_only)    return result
+if name == "__main__":
+    user_input = input("Введите строку: ")    result_string = display_letters(user_input)
+     if result_string:
+        print("Буквы в введенной строке:", result_string)    else:
+        print("В введенной строке нет букв.")
